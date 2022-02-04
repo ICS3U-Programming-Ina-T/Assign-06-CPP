558b6c0456fde2f67c0cee13bf011eb80708606b
Ubuntu <ubuntu@ip-172-31-5-22.ec2.internal>
2022-02-04 15:00:03 +0000

    Completed lists.cpp

---
+++ lists.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Ina Tolo All rights reserved.
+//
+// Created by: Ina Tolo
+// Created on: Jan. 28, 2022
+// This program gives the user for option
+// of playing three different games:
+// Palindrome Detector, Intgers and Multipliers,
+// and Index Size. Lists are used to
+// complete tasks.
+
